a71022c62d6f9908c53f58ae0cc99e15fc395d01
junyoung4213 <junyoung4213@gmail.com>
2020-05-02 01:27:12 +0900

    Ex2

---
+++ springProjectEx2_self/src/main/java/com/javalec/ex/MainClass.java
@@ -0,0 +1,19 @@
+package com.javalec.ex;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+	public static void main(String[] args) {
+		
+		String configPath = "classpath:applicationCTX.xml";
+		
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext(configPath);
+		MyInfo myInfo =  ctx.getBean("myInfo",MyInfo.class);
+		
+		myInfo.getInfo();
+		
+		ctx.close();
+		
+	}
+}
